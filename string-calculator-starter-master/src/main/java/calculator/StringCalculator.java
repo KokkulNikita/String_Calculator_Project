c3bdc61a48efdee37e5dc9437a4e673133314ead
KokkulNikita <kokkulnikita@gmail.com>
2021-10-17 16:25:37 +0530

    eight commit

--- string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
+++ string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
@@ -4,7 +4,7 @@ class StringCalculator {
 	
 	private final String delimiter =",|\n" ;
 
-    public int add(String input) {
+    public int add(String input) throws Exception {
     	String[] numbers =input.split(delimiter);
     	
     	if(isEmpty(input)) {
@@ -19,17 +19,31 @@ class StringCalculator {
     	}
     }
     
-    private int sum(String[] numbers) {
-    	int addition=0;
-    	for(String current:numbers) {
-    		if(stringToInt(current)>1000) {
-    			continue;
-    		}
-    	addition=addition+ stringToInt(current);
-    	}
-    	return addition;
+    private int sum(String[] numbers) throws Exception {
+    	findException(numbers);
+    	
+    	return calculateValues(numbers);
     }
     
+    private int calculateValues(String[] numbers) {
+    int addition=0;
+	for(String current:numbers) {
+			if(stringToInt(current)>1000) {
+				continue;
+			}
+			addition=addition+ stringToInt(current);
+		}
+	return addition;
+    }
+    
+    private void findException(String[] numbers) throws Exception {
+    for(String current:numbers) {
+		if(stringToInt(current)<0) {
+			throw new Exception("Negative input");
+		}
+	}
+    }
+   
     private boolean isEmpty(String input) {
     	return input.isEmpty();
     }
--- string-calculator-starter-master/src/test/java/calculator/StringCalculatorShould.java
+++ string-calculator-starter-master/src/test/java/calculator/StringCalculatorShould.java
@@ -12,39 +12,35 @@ class StringCalculatorShould {
 	private StringCalculator stringCalculator=new StringCalculator();
 	  
     @Test
-    void empty_string_should_return_0() {
+    void empty_string_should_return_0() throws Exception {
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
-    void string_with_single_number_should_return_number_as_int() {
+    void string_with_single_number_should_return_number_as_int() throws Exception {
           assertEquals(1, stringCalculator.add("1"));
     }
     
     @Test
-    public  void two_Numbers_Comma_Delimited_Return_Sum() {
+    public  void two_Numbers_Comma_Delimited_Return_Sum() throws Exception {
           assertEquals(4, stringCalculator.add("2,2"));
     }
     
     @Test
-    public  void two_Numbers_NewLine_Delimited_Return_Sum() {
+    public  void two_Numbers_NewLine_Delimited_Return_Sum() throws Exception {
           assertEquals(6, stringCalculator.add("1\n5"));
     }
     
     @Test
-    public  void three_Numbers_Delimited_Return_Sum() {
+    public  void three_Numbers_Delimited_Return_Sum() throws Exception {
           assertEquals(8, stringCalculator.add("1,2,5"));
     }
     
     @Test
-    public  void GreaterThan_1000_Ignore() {
+    public  void GreaterThan_1000_Ignore() throws Exception {
           assertEquals(6, stringCalculator.add("1,2000,5"));
     }
     
-//    @Test(expected =Exception.class)
-//    	public void negative_Input_Returns_Exception() {
-//    	stringCalculator.add("-1");
-//    }
-    
+
     
 }
